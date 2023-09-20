5892e99212e79aba6fde91659a4fea2c43709936
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-20 08:52:38 -0300

    UPDATE: Component table created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,34 @@
 import Layout from '@/components/Layout'
+import Tabela from '@/components/Tabela'
+import Cliente from '@/core/Cliente'
 
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Carlos', 12, '2'),
+    new Cliente('Beca', 65, '3'),
+    new Cliente('Gilson', 26, '4'),
+    new Cliente('Agatha', 40, '5'),
+
+  ]
+
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+    <div className={`
+    flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white
+  `}>
 
-   <Layout titulo='Cadastro Simples'>
-    <span>Conteudo</span>
-   </Layout>
+      <Layout titulo='Cadastro Simples'>
 
-   </div>
+        <Tabela clientes={clientes}> </Tabela >
+
+       
+
+      </Layout>
+    </div>
   )
 }
